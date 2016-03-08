b282c8d4646afc17f20aa2e9b93e5f7bb564e370
Mahmoud El-Zouhery <mahmoud.elzouhery@vodafone.com>
2016-03-08 09:37:12 +0200

    Encapsulate the get model in a separate service and use it in bre service

--- app/modules/BRE/bre.service.js
+++ app/modules/BRE/bre.service.js
@@ -1,49 +1,19 @@
-(function() {
+(function () {
     "use strict";
 
     angular
         .module('myApp')
-        .factory('breService', ['ruleEngineService', breService]);
+        .factory('breService', ['ruleEngineService', 'modelFactory', breService]);
 
-    function breService(ruleEngineService) {
-
-        //var result = JSONPath({json: model, path: '$.store.book[*].author', resultType: 'all'});
-
-            return {
-                getAddons:getAddons
-            }
-
-            function getAddons(){
-
-                var entities = ruleEngineService.getEntities();
-
-                //construct the model structure
-                var model = {
-                    addonTypes:{},
-                        addons:{
-                        promotions:getAddonGroup('promotion', entities),
-                        discounts:getAddonGroup('discount', entities)
-                    }
-                }
-
-
-               return model;
-            }
-
-            function getAddonGroup(groupName, entities){
-                var group = [];
-                var groupIndex = _.findIndex(entities.containers, function(o) { return o.name == groupName; });
-                _.forEach(entities.containers[groupIndex].entities, function(obj) {
-                    //fetch CMS and vlux data
-                    var addon = {
-                        name: obj.name,
-                        label: obj.name, //to be replaced by CMS labels
-                        value: ''
-                    }
-                    group.push(addon);
-                });
-                return group;
-            }
+    function breService(ruleEngineService, modelFactory) {
+        return {
+            getAddons: getAddons
         }
 
+        function getAddons() {
+            //construct the model structure
+            var model = modelFactory.getModel('AddOns');
+            return model;
+        }
+    }
 })();
